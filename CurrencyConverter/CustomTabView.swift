c489c02e4c16b401aca6b31049b0d80cbfd53e38
charan1435 <sc16101@gmail.com>
2024-11-28 16:22:54 +0530

    Tab View Initialization

--- CurrencyConverter/CustomTabView.swift
+++ CurrencyConverter/CustomTabView.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct CustomTabView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        TabView{
+            ContentView()
+                .tabItem(){
+                    Label("Home",systemImage: "house")
+                }
+            SettingsView()
+                .tabItem(){
+                    Label("Settings",systemImage: "gear")
+                }
+            
+        }
     }
 }
 
