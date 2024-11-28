19804a93fb912cb5842b4791428f8ab8bdf227b3
charan1435 <sc16101@gmail.com>
2024-11-28 16:22:22 +0530

    First Commit

---
+++ CurrencyConverter/CustomTabView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabView.swift
+//  CurrencyConverter
+//
+//  Created by Sabith Salman on 2024-11-28.
+//
+
+import SwiftUI
+
+struct CustomTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabView()
+}
