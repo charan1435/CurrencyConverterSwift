3306450a07b5d8d93e4d5230991cd7abf567619e
charan1435 <sc16101@gmail.com>
2024-11-28 16:11:53 +0530

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Sabith Salman on 2024-11-28.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CurrencyConverterTests/CurrencyConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterTests.swift
+//  CurrencyConverterTests
+//
+//  Created by Sabith Salman on 2024-11-28.
+//
+
+import Testing
+@testable import CurrencyConverter
+
+struct CurrencyConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
